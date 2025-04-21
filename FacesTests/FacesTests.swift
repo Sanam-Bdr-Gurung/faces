1f4f87571c0fbdad8e865370f0e464bdd227b7f3
Sanam <sanam@goodvibesofficial.com>
2025-04-21 13:00:11 +0545

    Initial Commit

---
+++ FacesTests/FacesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacesTests.swift
+//  FacesTests
+//
+//  Created by Sanam Gurung  on 21/04/2025.
+//
+
+import Testing
+@testable import Faces
+
+struct FacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
