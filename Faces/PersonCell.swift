98cc6800c6e998f05a80d6e56a4bf02fc9afc767
Sanam Bdr Gurung <sanam.bdr.gurung@gmail.com>
2025-04-28 12:56:24 +0545

    confirm to image protocol and import, convert and save to documents

---
+++ Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Faces
+//
+//  Created by Sanam Gurung  on 21/04/2025.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
